3d0a8ce17e4056ea789962b4d4c6315864ee0273
Adrians Riekstins 201RBC027 <124619408+RiekstinsA@users.noreply.github.com>
2023-04-17 13:23:50 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,61 @@
-# python3
+def read_file_input(file):
+    try:
+        with open(f"./tests/{file}") as f:
+            contents = f.readlines()
+    except FileNotFoundError:
+        raise ValueError("file doesnt exist")
+    except:
+        raise ValueError("read error")
+    
+    pattern = contents[0].strip()
+    text = contents[1].strip()
 
-def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+    return pattern, text
+
+def i_or_f():
+    input_i_or_f = input().rstrip()
     
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    if input_i_or_f == 'I':
+        return i_or_f()
+    elif input_i_or_f == 'F':
+        file = "06"
+        if str(file[-1]) == "a":
+            raise ValueError("file read error")
+        return read_file_input(file)
+    else:
+        raise ValueError("invalid input")
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
-    print(' '.join(map(str, output)))
+   print(' '.join(map(str, output)))
 
-def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+def get_occurrences(p, text):
 
-    # and return an iterable variable
-    return [0]
+    pattern = 1000000007
+    x = 1
+    result = []
 
+    pattern_hash = 0
+    for i in range(len(p)):
+            pattern_hash = (pattern_hash + ord(p[i]) * x) %pattern
+            x = (x * 263) %pattern
 
-# this part launches the functions
+    x = 1
+    sstr_hash = 0
+    for i in range(len(p)):
+        sstr_hash = (sstr_hash + ord(text[i]) * x) %pattern
+        x = (x * 263) %pattern
+
+    if pattern_hash == sstr_hash and pattern == text[:len(p)]:
+        result.apend(0)
+
+    for i in range(len(p), len(text)):
+        sstr_hash = (sstr_hash - ord(text[i - len(p)]) * x) %pattern
+        sstr_hash = (sstr_hash * 263 + ord(text[i])) %pattern
+
+        if sstr_hash ==pattern_hash and pattern == text[i - len(p) + 1:i + 1]:
+            result.apend(i - len(p) + 1)
+
+    return result
+
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
-
+   print_occurrences(get_occurrences(*i_or_f()))
